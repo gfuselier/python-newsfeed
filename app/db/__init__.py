25bdf7aaa8cb1f5a53438c18bc7e4ef5f778b4fd
gfuselier <gisellefuselier@gmail.com>
2024-01-19 23:35:24 -0600

    created database and user, post, comment, vote models with seed data

---
+++ app/db/__init__.py
@@ -0,0 +1,15 @@
+from os import getenv
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# connect to database using env variable
+# engine variable manages the overall connection to the database.
+engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
+# session variable generates temporary connections for performing create, read, update, and delete (CRUD) operations.
+Session = sessionmaker(bind=engine)
+# base class variable helps us map the models to real MySQL tables. 
+Base = declarative_base()
