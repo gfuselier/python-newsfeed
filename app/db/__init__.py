bc1cab4ff85e0f6b5c21a65233a3f604edb2fe4c
gfuselier <gisellefuselier@gmail.com>
2024-01-20 21:01:54 -0600

    set up database connection inside app

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from flask import g
 
 load_dotenv()
 
@@ -13,3 +14,19 @@ engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0
 Session = sessionmaker(bind=engine)
 # base class variable helps us map the models to real MySQL tables. 
 Base = declarative_base()
+
+def init_db(app):
+  Base.metadata.create_all(engine)
+
+  app.teardown_appcontext(close_db)
+def get_db():
+  if 'db' not in g:
+    # store db connection in app context
+    g.db = Session()
+def close_db(e=None):
+  db = g.pop('db', None)
+
+  if db is not None:
+    db.close()
+
+  return g.db
